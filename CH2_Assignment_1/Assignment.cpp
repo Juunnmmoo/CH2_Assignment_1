4ee8d54b2c021c4814aecd5096821912be2b9701
JunmoYang <115628899+Juunnmmoo@users.noreply.github.com>
2024-12-27 14:11:32 +0900

    Add Func

--- CH2_Assignment_1/Assignment.cpp
+++ CH2_Assignment_1/Assignment.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 vector<int> arr_int(5);
+int sum = 0;
+int average = 0;
 
 void Ascending()
 {
@@ -19,6 +21,10 @@ void Ascending()
 			arr_int[j + 1] = temp_int;
 		}
 	}
+
+	cout << "오름차순 정리 : ";
+	for (int i = 0; i < arr_int.size();i++)
+		cout << arr_int[i] << " ";
 }
 
 void descending()
@@ -35,6 +41,26 @@ void descending()
 			arr_int[j + 1] = temp_int;
 		}
 	}
+
+	cout << "내림차순 정리 : ";
+	for (int i = 0; i < arr_int.size();i++)
+		cout << arr_int[i] << " ";
+}
+
+void fSum()
+{
+	for (int i = 0; i < arr_int.size();i++)
+		sum += arr_int[i];
+
+	cout << "합계 : " << sum << endl;
+}
+
+void fAverage()
+{
+	average = sum / arr_int.size();
+
+	cout << "평균 : " << average << endl;
+
 }
 
 int main(void)
@@ -48,14 +74,13 @@ int main(void)
 
 	cin >> input_int;
 
+	fSum();
+	fAverage();
+
 	if (input_int == 1)
 		Ascending();
 	else if(input_int == 2)
 		descending();
 	
-
-	for (int i = 0; i < arr_int.size();i++)
-		cout << arr_int[i] << " ";
-
 	return 0;
 }
